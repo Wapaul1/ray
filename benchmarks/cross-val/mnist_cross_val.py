88da5df45d9b5ecbbe6cbe1486a48eb4c625f78a
William Paul <wapaul1@berkeley.edu>
2016-12-28 06:05:01 +0000

    Added timing

--- benchmarks/cross-val/mnist_cross_val.py
+++ benchmarks/cross-val/mnist_cross_val.py
@@ -2,9 +2,18 @@ from tensorflow.examples.tutorials.mnist import input_data
 import ray
 from sklearn import svm
 import ray_cross_val
+import time
+from sklearn.model_selection import cross_val_score
 
+times = []
 mnist = input_data.read_data_sets("MNIST_data/")
-
-ray.init(start_ray_local=True, num_workers=10)
-
-print ray_cross_val.ray_cross_val_score(svm.SVC(), mnist.test.images, mnist.test.labels)
+for i in range(1,3):
+  ray.init(start_ray_local=True, num_workers=10, num_local_schedulers=i)
+  before = time.time()
+  result = ray_cross_val.ray_cross_val_score(svm.SVC(), mnist.test.images, mnist.test.labels, cv=20)
+  times.append(time.time() - before)
+  before2 = time.time()
+  result2 = cross_val_score(svm.SVC(), mnist.test.images, mnist.test.labels, cv=20, n_jobs=-1)
+  after = time.time() - before2
+  print times[-1], after, result, result2
+  ray.worker.cleanup()
