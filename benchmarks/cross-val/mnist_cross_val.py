9bbe15ea80220d4bba012416b4b3d04e612ad200
William Paul <wapaul1@berkeley.edu>
2017-01-02 08:34:19 +0000

    Further changes

--- benchmarks/cross-val/mnist_cross_val.py
+++ benchmarks/cross-val/mnist_cross_val.py
@@ -16,11 +16,11 @@ workers = [2*i for i in range(1,9)]
 mnist = input_data.read_data_sets("MNIST_data/")
 for fold in folds:
   before = time.time()
-  result = cross_val_score(svm.SVC(), mnist.test.images, mnist.test.labels, cv=fold, n_jobs=-1)
+#  result = cross_val_score(svm.SVC(), mnist.test.images, mnist.test.labels, cv=fold, n_jobs=-1)
   after = time.time() - before
   times_skl_folds.append(after)
   print "SKL_folds ", times_skl_folds
-data = ray.init(start_ray_local=True, num_workers=10)
+data = ray.init(start_ray_local=True, num_workers=2)
 print data
 ray.register_class(type(svm.SVC()))
 ray.register_class(type(check_scoring(svm.SVC())), pickle=True)
