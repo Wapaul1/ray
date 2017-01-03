540b2c5cb6248102bdbfafaa3b40b973a967585b
William Paul <wapaul1@berkeley.edu>
2017-01-03 22:35:50 +0000

    Added timing for copying argument vs not copying

---
+++ test/timing.py
@@ -0,0 +1,25 @@
+import ray
+import numpy as np
+import copy
+import time
+
+@ray.remote
+def test_nocopy(X):
+  before = time.time()
+  result = (2*X).max()
+  after = time.time() - before
+  return after
+
+@ray.remote
+def test_copy(X):
+  X = copy.deepcopy(X)
+  before = time.time()
+  result = (2*X).max()
+  after = time.time() - before
+  return after
+
+ray.init(start_ray_local=True)
+
+array = np.random.randint(0, 100, (100000, 784))
+print np.mean([ray.get(test_nocopy.remote(array)) for _ in range(100)])
+print np.mean([ray.get(test_copy.remote(array)) for _ in range(100)])
